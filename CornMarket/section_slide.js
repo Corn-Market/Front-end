cf7926dbee582e1d5146ec287974171644118fcf
kimyounhyeon <fourkimm@naver.com>
2022-12-12 15:47:28 +0900

    ver1

---
+++ CornMarket/section_slide.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  autoplay:{
+      delay:3000
+  },
+  speed:1000
+  });
+});
