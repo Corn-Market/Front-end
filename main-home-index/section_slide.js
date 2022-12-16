c533492cc7dfb53246f5d2f197748b0dc6f7e296
guabaaa <gdandtop@naver.com>
2022-12-16 13:01:15 +0900

    1215555

--- main-home-index/section_slide.js
+++ main-home-index/section_slide.js
@@ -1,35 +1,34 @@
 
 window.addEventListener("load", function () {
-
-  let slides = document.querySelectorAll(".swiper .swiper-wrap .swiper-slide");
-  let contents = [];  //배열
+  let slides = document.querySelectorAll(".swiper-slide");
+  let contents = []; //배열
   slides.forEach(function (slide, index) {
-    contents[index] = slide.innerHTML;
-    //alert(contents[index]);
+    contents[index] = slide.outerHTML;
   });
-  
+
   let index = 0;
   function nextSlide() {
     index++;
-    if (index >= contents.length)
-      index = 0;
+    if (index >= contents.length) {
+      index = 1;
+    }
 
-    let swiper = document.querySelector(".swiper .swiper-wrap");
-    //alert(siper-wrap);
-    swiper.innerHTML = contents[index];
+    let swiper = document.querySelector(".swiper-wrap");
 
-    //애니메이션 효과 
-    swiper.animate({
-      transform: [
-        'translateX(100%)',    //시작
-        'translateX(0px)'      //도착
-      ]
-    },
+    //애니메이션 효과
+    swiper.animate(
       {
-        duration: 1000               // 위의효과가 1초간 지속되도록
-      });
+        transform: [
+          `translateX(${(index - 1) * -100}vw)`, //시작
+          `translateX(${index * -100}vw)`, //도착
+        ],
+      },
+      {
+        duration: 1000, // 위의효과가 1초간 지속되도록
+        fill: "forwards",
+      }
+    );
   }
 
   setInterval(nextSlide, 3000);
-
 });
