8d3237fe68d08d3c9810de3a4fd25baae40f42a2
guabaaa <gdandtop@naver.com>
2022-12-18 00:46:46 +0900

    1218searchmodify

--- search-id-pw/popup.js
+++ search-id-pw/popup.js
@@ -1,13 +1,13 @@
 function idPopup(){
     var url = "id-success-popup.html";
     var name = "email-code-popup";
-    var option = "width = 450 , height = 300"
+    var option = "width = 450 , height = 350"
     window.open(url,name,option);
 }
 function pwPopup(){
     var url = "pw-success-popup.html";
     var name = "email-code-popup";
-    var option = "width = 450 , height = 300"
+    var option = "width = 450 , height = 350"
     window.open(url,name,option);
 }
 
